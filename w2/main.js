7ac839b4e51a20e773a8d8a9519cb06518971adc
eric yang <ericy1410@gmail.com>
2024-08-22 20:03:17 -0700

    setup done

---
+++ w2/main.js
@@ -0,0 +1,19 @@
+// 1. count the members of your household.  
+const myhousemembers = 2;
+// 2. size of your home.
+const myHouseSize = 7;
+// 3. evaluate food choices.
+const myFoodChoices = 8;
+// 4. water consumption.
+const myWaterConsumption = 1;
+// 5. household purchases each year.
+const myHouseholdpurchases = 2;
+// 6. how much waste produced.
+const myWasteProduced = 30;
+// 7. your amount of waste recycled.
+const myWasteRecycled = 8;
+// 8. Tally up your annual transportation scores.
+const myTransportation = 4;
+// 9. add up your points.
+const cfptotal = "62";
+//10. write JS to update the rendered html (index.html) with total footprint.
