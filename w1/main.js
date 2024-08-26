9836624ad6d251780624096dd4456d543378189e
eric yang <ericy1410@gmail.com>
2024-08-26 12:07:27 -0700

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,25 @@
-console.log('hello World!')
+console.log("hello from inside the main.js file");
+
+//let myVar;
+
+const myVar = "eric";
+const myVarType = typeof(myVar)
+console.log("myVarType" + myVarType);
+console.log(`myVarType ${myVarType}`);
+
+if(myVarType === "number") {
+    console.log (`will 11 this one run?`)
+}else {
+    console.log(`will 13 this one run?`)
+}
+
+function runNow () {
+    if(myVarType === "number") {
+        console.log (`will 11 this one run?`)
+    }else {
+        console.log(`will 13 this one run?`)
+    }
+}
+
+runNow()
+runNow()
