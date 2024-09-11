52e1e89f023b4ffc8fe3a9aa3a21bcaf29c6d1ef
eric yang <ericy1410@gmail.com>
2024-09-11 10:37:14 -0700

    setup and array of objects

--- w4/main.js
+++ w4/main.js
@@ -42,64 +42,74 @@ function determineHouseHoldPts(numberInHousehold) {
      
 
 
-console.log("global scope");
+function displayOutObj(obj) {
+    console.log(obj)
+    const output = document.getElementById("output");
+    const newH2 = document.createElement("h2");
+    newH2.textContent = `carbon FootPrint ${obj.cfpTotal}`;
+    const newH3 = document.createElement("h3");
+    newH3.textContent = `based on number in and size of home`
+    const newP = document.createElement("p");
+    newP.textContent = `this number is based on the number of people in the house of ${obj.cfpTotal}.`;
 
+    output.appendChild(newH3);
+    output.appendChild(newH2);
 
+}
 
 function start(houseHoldMembers, houseSize) {
     const householdPTS = determineHouseHoldPts(houseHoldMembers);
     const houseSizePTS = determineHouseSizePts(houseSize);
     const total = householdPTS + houseSizePTS;
-    cfpData.push([houseHoldMembers, houseSize, householdPTS, houseSizePTS, total])
-    console.log(cfpData);
+    const cfpObj = {
+        houseMembers: houseHoldMembers,
+        houseS: houseSize,
+        houseMembersPTs: householdPTS,
+        houseSPTS: houseSizePTS,
+        cfpTotal: total
+    }
+    displayOutObj(cfpObj);
 
-    console.log(`total carbon footprint score so far ${total}`)
- }
+}
+
+
+
+   // console.log(`total carbon footprint score so far ${total}`)
+ //}
 
 //last code along for week 4- i think it does because it helps to run it correctly
 
  //const myInputs = [5, "apt"]; // on the global scope
  
- function displayOutput(){
-    for (arr of cfpData){
-        console.log(arr)
-        const output = document.getElementById("output");
-        const newP1 = document.createElement("P")
-        const newP2 = document.createElement("P")
-        const newP3 = document.createElement("P")
-        const newP4 = document.createElement("P")
-        const newP5 = document.createElement("P")
-        const newP6 = document.createElement("P")
-        newP1.textContent = `Carbon Footprint total is ${arr[4]}`;
-        newP2.textContent = `number of household members is ${arr[2]}`
-        newP3.textContent =  `the score of house members is ${arr[2]}}`
-        newP4.textContent =  `the size of house is ${arr[1]}`
-        newP5.textContent =  `score of the house is ${arr[3]}`
+ //function displayOutput(){
+   // for (arr of cfpData){
+        //console.log(arr)
+        //const output = document.getElementById("output");
+        //const newP1 = document.createElement("P")
+        //const newP2 = document.createElement("P")
+       // const newP3 = document.createElement("P")
+        //const newP4 = document.createElement("P")
+       // const newP5 = document.createElement("P")
+       // const newP6 = document.createElement("P")
+        //newP1.textContent = `Carbon Footprint total is ${arr[4]}`;
+       // newP2.textContent = `number of household members is ${arr[2]}`
+       // newP3.textContent =  `the score of house members is ${arr[2]}}`
+       // newP4.textContent =  `the size of house is ${arr[1]}`
+       // newP5.textContent =  `score of the house is ${arr[3]}`
 
-     output.appendChild(newP1);
-     output.appendChild(newP2);
-     output.appendChild(newP3);
-     output.appendChild(newP4);
-     output.appendChild(newP5);
-    }
- }
+     //output.appendChild(newP1);
+     //output.appendChild(newP2);
+     //output.appendChild(newP3);
+    // output.appendChild(newP4);
+     //output.appendChild(newP5);
+   // }
+// }
 
 start(5, "apt");
 start(4, "large");
 start(3, "medium");
 start(2, "small");
 start(2, "apt")
-
-
-start(8, "large")
-start(4, "apt")
-start(1, "medium")
-start(2, "small")
-displayOutput()
-
- 
-
-
 
 
  // not going to read inside the block
@@ -122,3 +132,31 @@ displayOutput()
 
  // I think i need to go back and rewatch the video but i did get a good idea of it
  //
+
+
+
+
+
+ //function displayoutput() {
+   // for (arr of cfpData) {
+      //  const output = document.getElementById("output");
+       // const newH2 = document.createElement("h2");
+       // newH2.textContent = `carbon FootPrint ${arr[4]}`;
+        //const newH3 = document.createElement("h3");
+       // newH3.textContent = `based on number in and size of home`
+       // const newP = document.createElement("p");
+       // newP.textContent = `this number is based on the number of people in the house of ${arr[2]}.`;
+       // newP.textContent += `and a ${arr[1]} size of home (score:${arr[2]}).`;
+       // output.appendChild(newH2);
+       // output.appendChild(newH3);
+       // output.appendChild(newP);
+    //}
+// }
+
+
+
+//const cfpData = [{
+
+//} {
+
+//}]
---
+++ w5/main.js
@@ -0,0 +1,77 @@
+const cfpData = []
+
+function determineHouseSizePts(size) {
+ let housesizePoints = 0;
+    if (size === "Large") {
+        housesizePoints = 10;
+    } else if (size === "medium") {
+        housesizePoints = 7;
+    } else if (size === "small") {
+        housesizePoints = 4;
+    } else if (size === "apt") {
+        housesizePoints = 2;
+    }
+    return housesizePoints
+}
+
+
+function determineHouseHoldPts(numberInHousehold) {
+    console.log("inside block scope");
+    let houseHoldPoints = 0;
+    if (numberInHousehold === 1) {
+        houseHoldPoints = 14
+    }else if (numberInHousehold === 2) {
+        houseHoldPoints = 12
+    }else if (numberInHousehold === 3) { 
+        houseHoldPoints = 10
+    }else if (numberInHousehold === 4) {
+        houseHoldPoints = 8
+    }else if (numberInHousehold === 5) {
+        houseHoldPoints = 6
+    }else if (numberInHousehold === 6) {
+        houseHoldPoints = 4
+    }else if (numberInHousehold > 6) {
+        houseHoldPoints = 2
+    }
+   
+    return houseHoldPoints
+}
+     
+function start(houseHoldMembers, houseSize) {
+    const householdPTS = determineHouseHoldPts(houseHoldMembers);
+    const houseSizePTS = determineHouseSizePts(houseSize);
+    const total = householdPTS + houseSizePTS;
+
+    cfpData.push({
+        houseMembers: houseHoldMembers,
+        houseS: houseSize,
+        houseMembersPTs: householdPTS,
+        houseSPTS: houseSizePTS,
+        cfpTotal: total
+    } );
+}
+
+
+ function displayoutput() {
+   for (obj of cfpData) {
+    console.log(obj)
+      const output = document.getElementById("output");
+       const newH2 = document.createElement("h2");
+        newH2.textContent = `carbon FootPrint ${obj.cfpTotal}`;
+        const newH3 = document.createElement("h3");
+       newH3.textContent = `based on number in and size of home`
+       const newP = document.createElement("p");
+       newP.textContent = `this number is based on the number of people in the house of ${obj.houseHoldMembers} (score ${obj.houseHoldPTS}).`;
+       newP.textContent += `and a ${obj.houseSize} size of home (score:${obj.housesizePoints}).`;
+       output.appendChild(newH2);
+       output.appendChild(newH3);
+       output.appendChild(newP);
+    }
+ }
+start(5, "apt");
+start(4, "large");
+start(3, "medium");
+start(2, "small");
+
+
+displayoutput()
